403266935a6b063c86519bcf39c40d06c2754e5b
Tanya Kogan <tanyak05@gmail.com>
2020-08-30 10:45:00 -0400

    Code refactoring and adding explanations and comments

--- calculations/calculations.py
+++ calculations/calculations.py
@@ -1,21 +1,39 @@
-import base64
 import numpy as np
 
 
+# This is the file that combines all calculations methods needed for the REST API point
+# Note: in Java or C# I'll make it like a static class, but I see no point to do this in Python
+# (though maybe there may be loading implications, like loading too much methods, the methods violation etc.
+# since I know that Python is very flexible)
+
+# Method that calculates spectrum
+# Problems that I can not fix:
+# 1. There is not used parameter 'units'
+# 2. There not used 'duration'
+# 3. Calculations that unnecessary (lines 2 and 3)
+# 4. Method takes 'spectrum', reformat it and returns back the reformation - what is the purpose of the method?
 def calc_spectrum(sampling_rate, units, dtype, spectrum_):
-    spectrum = np.frombuffer(base64.b64decode(spectrum_.encode('utf-8')), dtype=dtype).tolist()
-    samples_per_signal = len(spectrum)
+    spectrum = np.frombuffer(spectrum_, dtype=dtype).tolist()
+    samples_per_signal = len(spectrum)  # why do we need this?
     duration = samples_per_signal / sampling_rate
     return spectrum
 
 
-def calculate_mean(spectrum):
-    samples_per_signal = len(spectrum)
-    mean = np.mean(spectrum)
+# Method that calculates mean
+# Problems that I can not fix:
+# 1. There is not used 'samples_per_signal'
+def calculate_mean(spectrum, dtype):
+    spectrum_ = np.frombuffer(spectrum, dtype=dtype).tolist()
+    samples_per_signal = len(spectrum_)
+    mean = np.mean(spectrum_)
     return mean
 
 
-def calculate_median(spectrum):
+# Method that calculates mean
+# Problems that I can not fix:
+# 1. There is not used 'samples_per_signal'
+def calculate_median(spectrum, dtype):
+    spectrum_ = np.frombuffer(spectrum, dtype=dtype).tolist()
     samples_per_signal = len(spectrum)
     median = np.median(spectrum)
     return median
